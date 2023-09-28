9d0afea316a6ceaf700df933091eb371950990cb
Alan <alan.rente@gmail.com>
2023-09-28 20:46:43 -0300

    feat: conection in database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
+import { config } from "dotenv";
+config();
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import routes from "./routes";
-dotenv.config();
+import { sequelize } from "./infra/database";
 const app = express();
 
 app.use(cors());
+
+try {
+  sequelize.authenticate().then(() => {
+    console.debug("Conectado ao banco");
+  });
+} catch (error) {
+  console.error("Não foi possível conectar ao banco", error);
+}
 
 app.get("/", (req, res) => {
   res.json({ message: "rota / metodo get" });
---
+++ src/infra/database.ts
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize";
+
+const { database, username, password, host } = {
+  database: `${process.env.DB_DATABASE}`,
+  username: `${process.env.DB_USERNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
+  host: `${process.env.DB_HOST}`,
+};
+
+export const sequelize = new Sequelize(database, username, password, {
+  dialect: "postgres",
+  host: host,
+  schema: process.env.DB_SCHEMA,
+});
